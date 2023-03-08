3a9aee96e0e225e7820da08d594df16f204d81af
Kirill Plokhotnichenko <www.origin@bk.ru>
2023-03-08 22:23:13 +0500

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число : ");
+int UserNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Таблица кубов: ");
+for (int i = 1; i <= UserNumber; i++)
+{
+    int Cube = i * i * i;
+    Console.Write($"{Cube}, ");
+}
